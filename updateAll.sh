15fa00e1fc3ba0665ac5495e3f78ff5283f10967
Kostya <konstantin.tresko@ideaplatfrom.ru>
2017-12-01 15:57:29 +0300

    /

--- updateAll.sh
+++ updateAll.sh
@@ -1,5 +1,5 @@
-sshpass -p 'ipSy$tem1231' scp root@194.87.111.93:.usr/tmp/$1 /opt/landing_page
-echo -e "\e[1;34m Articles were copied from  root@194.87.111.93:.usr/tmp/$1 \e[0m"
+sshpass -p 'ipSy$tem1231' scp root@194.87.111.93:/usr/tmp/$1 /opt/landing_page
+echo -e "\e[1;34m Articles were copied from  root@194.87.111.93:/usr/tmp/$1 \e[0m"
 cd ref
 node saveFile ../$1
 echo -e "\e[1;34m Rebuilding client.. \e[0m"
